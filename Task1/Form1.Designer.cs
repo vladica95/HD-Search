73e056e936c6b2af763998a1a579c498b5ecf306
Vladica Vučić <vladica.vucic@elfak.rs>
2020-02-11 19:18:52 +0100

    Initial commit

---
+++ Task1/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxDrives = new System.Windows.Forms.ListBox();
+            this.lblDriveInfo = new System.Windows.Forms.Label();
+            this.txtDriveInfo = new System.Windows.Forms.TextBox();
+            this.tvTree = new System.Windows.Forms.TreeView();
+            this.txtItemName = new System.Windows.Forms.TextBox();
+            this.lblItemInfo = new System.Windows.Forms.Label();
+            this.lblFileType = new System.Windows.Forms.Label();
+            this.txtFileType = new System.Windows.Forms.TextBox();
+            this.btnFileFilter = new System.Windows.Forms.Button();
+            this.txtItemSize = new System.Windows.Forms.TextBox();
+            this.txtItemAttribute = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbxDrives
+            // 
+            this.lbxDrives.FormattingEnabled = true;
+            this.lbxDrives.Location = new System.Drawing.Point(12, 12);
+            this.lbxDrives.Name = "lbxDrives";
+            this.lbxDrives.Size = new System.Drawing.Size(151, 82);
+            this.lbxDrives.TabIndex = 0;
+            this.lbxDrives.SelectedIndexChanged += new System.EventHandler(this.lbxDrives_SelectedIndexChanged);
+            // 
+            // lblDriveInfo
+            // 
+            this.lblDriveInfo.AutoSize = true;
+            this.lblDriveInfo.Location = new System.Drawing.Point(182, 15);
+            this.lblDriveInfo.Name = "lblDriveInfo";
+            this.lblDriveInfo.Size = new System.Drawing.Size(64, 26);
+            this.lblDriveInfo.TabIndex = 1;
+            this.lblDriveInfo.Text = "Drive\r\ninformation :";
+            // 
+            // txtDriveInfo
+            // 
+            this.txtDriveInfo.AcceptsTab = true;
+            this.txtDriveInfo.Location = new System.Drawing.Point(252, 12);
+            this.txtDriveInfo.Multiline = true;
+            this.txtDriveInfo.Name = "txtDriveInfo";
+            this.txtDriveInfo.ReadOnly = true;
+            this.txtDriveInfo.Size = new System.Drawing.Size(214, 36);
+            this.txtDriveInfo.TabIndex = 2;
+            // 
+            // tvTree
+            // 
+            this.tvTree.Location = new System.Drawing.Point(12, 100);
+            this.tvTree.Name = "tvTree";
+            this.tvTree.Size = new System.Drawing.Size(151, 123);
+            this.tvTree.TabIndex = 3;
+            this.tvTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvTree_AfterSelect);
+            this.tvTree.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvTree_NodeMouseDoubleClick);
+            // 
+            // txtItemName
+            // 
+            this.txtItemName.AcceptsReturn = true;
+            this.txtItemName.AcceptsTab = true;
+            this.txtItemName.Location = new System.Drawing.Point(252, 54);
+            this.txtItemName.Multiline = true;
+            this.txtItemName.Name = "txtItemName";
+            this.txtItemName.ReadOnly = true;
+            this.txtItemName.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtItemName.Size = new System.Drawing.Size(214, 59);
+            this.txtItemName.TabIndex = 4;
+            // 
+            // lblItemInfo
+            // 
+            this.lblItemInfo.AutoSize = true;
+            this.lblItemInfo.Location = new System.Drawing.Point(182, 100);
+            this.lblItemInfo.Name = "lblItemInfo";
+            this.lblItemInfo.Size = new System.Drawing.Size(52, 26);
+            this.lblItemInfo.TabIndex = 5;
+            this.lblItemInfo.Text = "Selected \r\nitem info :";
+            // 
+            // lblFileType
+            // 
+            this.lblFileType.AutoSize = true;
+            this.lblFileType.Location = new System.Drawing.Point(182, 182);
+            this.lblFileType.Name = "lblFileType";
+            this.lblFileType.Size = new System.Drawing.Size(52, 13);
+            this.lblFileType.TabIndex = 6;
+            this.lblFileType.Text = "File type :";
+            // 
+            // txtFileType
+            // 
+            this.txtFileType.Location = new System.Drawing.Point(252, 179);
+            this.txtFileType.Name = "txtFileType";
+            this.txtFileType.Size = new System.Drawing.Size(81, 20);
+            this.txtFileType.TabIndex = 7;
+            // 
+            // btnFileFilter
+            // 
+            this.btnFileFilter.Location = new System.Drawing.Point(362, 168);
+            this.btnFileFilter.Name = "btnFileFilter";
+            this.btnFileFilter.Size = new System.Drawing.Size(104, 31);
+            this.btnFileFilter.TabIndex = 8;
+            this.btnFileFilter.Text = "Use filter";
+            this.btnFileFilter.UseVisualStyleBackColor = true;
+            this.btnFileFilter.Click += new System.EventHandler(this.btnFileFilter_Click);
+            // 
+            // txtItemSize
+            // 
+            this.txtItemSize.AcceptsReturn = true;
+            this.txtItemSize.AcceptsTab = true;
+            this.txtItemSize.Location = new System.Drawing.Point(252, 110);
+            this.txtItemSize.Multiline = true;
+            this.txtItemSize.Name = "txtItemSize";
+            this.txtItemSize.ReadOnly = true;
+            this.txtItemSize.Size = new System.Drawing.Size(214, 28);
+            this.txtItemSize.TabIndex = 9;
+            // 
+            // txtItemAttribute
+            // 
+            this.txtItemAttribute.AcceptsReturn = true;
+            this.txtItemAttribute.AcceptsTab = true;
+            this.txtItemAttribute.Location = new System.Drawing.Point(252, 135);
+            this.txtItemAttribute.Multiline = true;
+            this.txtItemAttribute.Name = "txtItemAttribute";
+            this.txtItemAttribute.ReadOnly = true;
+            this.txtItemAttribute.Size = new System.Drawing.Size(214, 27);
+            this.txtItemAttribute.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Please be patient while tree is loaded.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 235);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtItemAttribute);
+            this.Controls.Add(this.txtItemSize);
+            this.Controls.Add(this.btnFileFilter);
+            this.Controls.Add(this.txtFileType);
+            this.Controls.Add(this.lblFileType);
+            this.Controls.Add(this.lblItemInfo);
+            this.Controls.Add(this.txtItemName);
+            this.Controls.Add(this.tvTree);
+            this.Controls.Add(this.txtDriveInfo);
+            this.Controls.Add(this.lblDriveInfo);
+            this.Controls.Add(this.lbxDrives);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxDrives;
+        private System.Windows.Forms.Label lblDriveInfo;
+        private System.Windows.Forms.TextBox txtDriveInfo;
+        private System.Windows.Forms.TreeView tvTree;
+        private System.Windows.Forms.TextBox txtItemName;
+        private System.Windows.Forms.Label lblItemInfo;
+        private System.Windows.Forms.Label lblFileType;
+        private System.Windows.Forms.TextBox txtFileType;
+        private System.Windows.Forms.Button btnFileFilter;
+        private System.Windows.Forms.TextBox txtItemSize;
+        private System.Windows.Forms.TextBox txtItemAttribute;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Task1/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Specialized;
+
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        static StringCollection log = new StringCollection();
+        string filter = "*.*";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] drives = System.Environment.GetLogicalDrives();
+            foreach (string dr in drives)
+            {
+                DriveInfo driveInfo = new DriveInfo(dr);
+                lbxDrives.Items.Add(driveInfo);
+            }
+
+        }
+
+        private void ListDirectory(TreeView treeView,DirectoryInfo directoryInfo)
+        {
+            treeView.Nodes.Clear();
+            treeView.Nodes.Add(WalkDirectoryTree(directoryInfo));
+        }
+        
+        private TreeNode WalkDirectoryTree(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            try
+            {
+                foreach (var directory in directoryInfo.GetDirectories())
+                {
+                    directoryNode.Nodes.Add(WalkDirectoryTree(directory));
+                }
+                foreach (var file in directoryInfo.GetFiles(filter))
+                {
+                    directoryNode.Nodes.Add(new TreeNode(file.Name));
+                }
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                log.Add(e.Message);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                log.Add(e.Message);
+            }
+
+            return directoryNode;
+        }
+
+        private void lbxDrives_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ClearTextBox();
+                DriveInfoAndTree();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                log.Add(ex.Message);
+            }
+        }
+
+        private void DriveInfoTxtBox(DriveInfo drive)
+        {
+            txtDriveInfo.Text = "Drive letter: " + drive.Name.ElementAt(0) +
+                  "\t Drive name : " + drive.Name + "\t Free space : " + drive.AvailableFreeSpace / 1073741824
+                  + " GB" + "\t Total size : " + drive.TotalSize / 1073741824 + " GB";
+        }
+
+        private void ClearTextBox()
+        {
+            txtItemName.Text = "";
+            txtItemSize.Text = "";
+            txtItemAttribute.Text = "";
+        }
+
+        private void DriveInfoAndTree()
+        {
+            DriveInfo drive = (DriveInfo)lbxDrives.SelectedItem;
+            DriveInfoTxtBox(drive);
+            DirectoryInfo rootDir = drive.RootDirectory;
+            ListDirectory(tvTree, rootDir);
+        }
+
+        private void tvTree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string typeOfNode;
+            string secondDetail = " ";
+            string attribute = " ";
+            bool isNodeFolder = true;
+
+            if (!Directory.Exists(tvTree.SelectedNode.FullPath))
+            {
+                isNodeFolder = false;
+            }
+            if (!isNodeFolder)
+            {
+                var selectedDirectory = new DirectoryInfo(tvTree.SelectedNode.Parent.FullPath);
+                foreach (var file in selectedDirectory.GetFiles())
+                {
+                    if (tvTree.SelectedNode.Text == file.Name)
+                    {
+                        FileInfo selectedFile = file;
+                        secondDetail = "File size : " + (selectedFile.Length / 1024).ToString() + " KB";
+                        attribute = selectedFile.Attributes.ToString();
+                    }
+                }
+                typeOfNode = "File";
+                SelectedItemInfo(typeOfNode, secondDetail, attribute);
+            }
+            else
+            {
+                typeOfNode = "Folder";
+                var selectedDirectory = new DirectoryInfo(tvTree.SelectedNode.FullPath);
+                secondDetail = "Number of files : " + (tvTree.SelectedNode.GetNodeCount(true)).ToString();
+                attribute = selectedDirectory.Attributes.ToString();
+                SelectedItemInfo(typeOfNode, secondDetail, attribute);
+            }
+        }
+
+        private void SelectedItemInfo(string typeOfNode, string secondDetail, string attribute)
+        {
+            txtItemName.Text = "Name of " + typeOfNode + " : " + tvTree.SelectedNode.ToString().Replace("TreeNode: ", "");
+            txtItemSize.Text = secondDetail;
+            txtItemAttribute.Text = typeOfNode + " attribute: " + attribute;
+        }
+
+        private void tvTree_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            System.Diagnostics.Process.Start(tvTree.SelectedNode.FullPath);
+        }
+        
+        private void btnFileFilter_Click(object sender, EventArgs e)
+        {
+            if (lbxDrives.SelectedIndex == -1)
+            {
+                lbxDrives.SelectedIndex = 0;
+            }
+
+            if (txtFileType.Text == "" || txtFileType.Text == " ")
+            {
+                filter = "*.*";
+            }
+            else
+                filter = "*." + txtFileType.Text;
+            DriveInfoAndTree();
+        }
+    }
+}
